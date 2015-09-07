654e898f549637fee5d892031a0acc8d1d3ea623
s-nakajima <nakajimashouhei@gmail.com>
2015-09-07 16:39:50 +0900

    phpdoc修正

--- Controller/FaqQuestionOrdersController.php
+++ Controller/FaqQuestionOrdersController.php
@@ -3,7 +3,7 @@
  * FaqQuestionOrders Controller
  *
  * @author Noriko Arai <arai@nii.ac.jp>
- * @author Ryo Ozawa <ozawa.ryo@withone.co.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
  * @copyright Copyright 2014, NetCommons Project
@@ -14,7 +14,7 @@ App::uses('FaqsAppController', 'Faqs.Controller');
 /**
  * FaqQuestionOrders Controller
  *
- * @author Ryo Ozawa <ozawa.ryo@withone.co.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\Faqs\Controller
  */
 class FaqQuestionOrdersController extends FaqsAppController {
